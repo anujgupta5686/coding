596e5c91df1e8d38feeada80b8b4c3a5c6c3c5f9
anujgupta5686 <anujgupta5686@gmail.com>
2025-01-05 00:01:36 +0530

    I write logic or Patterns

---
+++ loop1.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // for(int i=0;i<5;i++){
+    //     cout<<i<<" "<<"-> ";
+    //     for(int j=0;j<5;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    // cout<<"============================================================="<<endl;
+    // cout<<"Square Pattern"<<endl<<"\n";
+    // for(int i=0;i<5;i++){
+    //     for(int j=0;j<5;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    // cout<<"Rectagle Pattern"<<endl<<"\n";
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<7;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    // cout<<"Hollow Rectagle Pattern"<<endl<<"\n";
+    // for(int i=0;i<4;i++){
+    //     for(int j=0;j<5;j++){
+    //         if(i==0 || i==3 || j==0 || j==4){
+    //             cout<<"* ";
+    //         }
+    //         else{
+    //             cout<<"  ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int n;
+    // cout<<"Enter Number: ";
+    // cin>>n;
+    // cout<<"Half Pyramid Pattern"<<endl<<"\n";
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<=i;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    // int n;
+    // cout<<"Enter Number: ";
+    // cin>>n;
+    // cout<<"Reverse Half Pyramid Pattern"<<endl<<"\n";
+    // for(int i=1;i<=n;i++){
+    //     // for(int j=1;j<=n-i+1;j++){
+    //     for(int j=i;j<=n;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    int n;
+    cout << "Enter Number: ";
+    cin >> n;
+    cout << "Hollow Half Pyramid Pattern" << endl
+         << "\n";
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if (i == 1 || i == 2 || i == n || j == 1 || j == i)
+            {
+                cout << "* ";
+            }
+            else
+            {
+
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num,num2;
+    cout << "Number 1 : ";
+    cin >> num;
+    cout << "Number 2 : ";
+    cin>>num2;
+    cout<<"Sum of the number is : "<<(num+num2)<<endl;
+    return 0;
+}
